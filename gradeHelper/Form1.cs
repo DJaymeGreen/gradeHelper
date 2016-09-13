49085420d84d20326e46c48507c16ad9be44cac0
D Jayme Green <Djaymegreen@gmail.com>
2016-09-12 20:54:09 -0400

    First Test Commit

--- gradeHelper/Form1.cs
+++ gradeHelper/Form1.cs
@@ -16,5 +16,10 @@ namespace gradeHelper
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is a test");
+        }
     }
 }
